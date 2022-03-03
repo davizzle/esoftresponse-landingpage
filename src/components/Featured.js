65c6ed0fd81e4cabf30e20211d35a77e838ad703
samsonajulor <samson.ajulor@decagon.dev>
2022-03-03 13:26:37 +0100

    created the featured

---
+++ src/components/Featured.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <>
+      <h2>Featured Work</h2>
+      <div>
+        <div>{/* Image */}</div>
+        <div>
+          <h4>Swap Stall Market Place</h4>
+          <p>This is a reasonable description</p>
+        </div>
+      </div>
+      <div>
+        {/* forwardIcon */}
+        {/* backwardIcon */}
+      </div>
+    </>
+  )
+}
+
+export default Featured
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Header from '../components/Header'
+import Expertise from '../components/Expertise'
+import Featured from '../components/Featured'
 
 function Home() {
   return (
+    <>
     <Nav/>
+    <Header/>
+    <Expertise/>
+    <Featured/>
+    </>
   )
 }
 

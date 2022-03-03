bea2e694ce5ed8319da9b461f28c6d268ca26d3e
samsonajulor <samson.ajulor@decagon.dev>
2022-03-03 14:00:44 +0100

    created the work comp

---
+++ src/components/Work.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ButtonDark from './ButtonDark'
+
+function Work() {
+  return (
+    <>
+     <h2>Our Work</h2>
+     <p>Cost Effective, Scalable, Flexible Solutions</p>
+     <div>
+      <div>
+       <div>
+        {/* Image */}
+       </div>
+       <ButtonDark/>
+      </div>
+      <div>
+       <h4>Tesla</h4>
+       <p>
+        This is some reasonable description
+       </p>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default Work

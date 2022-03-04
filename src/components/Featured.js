94e349657b3dc92c1967d3f0013bc1ca937c87ec
samsonajulor <samson.ajulor@decagon.dev>
2022-03-04 05:58:19 +0100

    styled expertise for desktop

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import ButtonDark from './ButtonDark'
+import phoneinhand from '../assets/images/phoneinhand.png'
 
 function Featured() {
   return (
     <>
-      <h2>Featured Work</h2>
-      <div>
-        <div>{/* Image */}</div>
-        <div>
-          <h4>Swap Stall Market Place</h4>
-          <p>This is a reasonable description</p>
+      <h2 className='featured__title'>Featured</h2>
+      <div className='featured__cont'>
+        <div className='featured__left'>
+          <h4>Project Name Here</h4>
+          <p>some text here to show that we know what we are doing</p>
+          <div>
+          <ButtonDark text={'Read more'} />
+          </div>
         </div>
-      </div>
-      <div>
-        {/* forwardIcon */}
-        {/* backwardIcon */}
+        <div className='featured__right'>
+          <img src={phoneinhand} alt='phone in hand' />
+        </div>
       </div>
     </>
   )
